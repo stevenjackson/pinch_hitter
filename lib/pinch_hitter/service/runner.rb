05805e1243498f60c7f0d32cf384d1804493affa
Steve Jackson <steve.jackson@leandogsoftware.com>
2015-08-14 22:30:40 -0400

    Add ability to silence the sintatra/webrick output
    pinch_hitter priming and serving adds a lot of noise to test logs.  Now
    you can call `silence_console` before starting the pinch_hitter service
    and snuff that noise.

--- lib/pinch_hitter/service/runner.rb
+++ lib/pinch_hitter/service/runner.rb
@@ -1,22 +1,36 @@
 require 'rack'
+require 'logger'
 require 'webrick'
 require 'pinch_hitter/service/replay_ws'
 
 module PinchHitter::Service::Runner
+  def silence_console
+    @silent_console = true
+  end
+
   def start_service(host, port, timeout=10)
     Thread.abort_on_exception = true
     @app = PinchHitter::Service::ReplayWs.new
     @replay_service = Thread.new do
-      Rack::Handler::WEBrick.run @app, :Host => host, :Port => port
+      Rack::Handler::WEBrick.run @app, service_options(host, port)
     end
     wait_for_replay(timeout)
+  end
+
+  def service_options(host, port)
+    {}.tap do |hash|
+      hash[:Host] = host
+      hash[:Port] = port
+      hash[:AccessLog] = [] if @silent_console
+      hash[:Logger] = SilentLogger.new if @silent_console
+    end
   end
 
   def stop_service
     @replay_service.kill
   end
 
-  private 
+  private
   def wait_for_replay(timeout)
     end_time = ::Time.now + timeout
     until ::Time.now > end_time
@@ -24,5 +38,12 @@ module PinchHitter::Service::Runner
       return if @replay_service.stop?
     end
     raise 'Timed out waiting for replay service to start'
+  end
+
+  #Borrowed from https://github.com/watir/watirspec/blob/master/lib/silent_logger.rb
+  class SilentLogger
+    (::Logger.instance_methods - Object.instance_methods).each do |logger_instance_method|
+      define_method(logger_instance_method) { |*args| }
+    end
   end
 end
